92af75413aa61f617797fd2d55764e5162ed8205
Cédric Delalande <cedric.delalande@gmail.com>
2012-01-07 20:19:10 +0100

    ignore test file

--- test.php
+++ test.php
@@ -4,6 +4,9 @@ namespace base;
 
 Class App
 {
+
+	private static $instance;
+	
 	function __construct()
 	{
 		$this->t = new \base\alpha\Two();
@@ -13,6 +16,26 @@ Class App
 	{
 		echo "Play Yvette";
 	}
+	public static function singleton()
+    {
+        if (!isset(self::$instance)) {
+            echo 'Creating new instance of : ';
+            $className = __CLASS__;
+            echo $className;
+            self::$instance = new $className;
+        }
+        return self::$instance;
+    }
+    
+    public function __clone()
+    {
+        trigger_error('Clone is not allowed.', E_USER_ERROR);
+    }
+
+    public function __wakeup()
+    {
+        trigger_error('Unserializing is not allowed.', E_USER_ERROR);
+    }
 }
 
 namespace base\alpha;
@@ -24,7 +47,6 @@ Class One Extends \base\App
 	{
 		echo "Class One loaded";
 		parent::play();
-		$a->play();
 	}
 }
 
@@ -32,10 +54,13 @@ namespace base\alpha;
 
 Class Two Extends One
 {
+	
 }
 
 use App;
-$a = new \base\App();
-
-
+$a = new \base\alpha\Two();
+echo "off";
+$a->play();
+$b = new \base\App();
+echo count(get_declared_classes());
 ?>
